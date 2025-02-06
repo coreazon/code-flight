09e35dc3de1fe530e082f1a12c71ec4b7fd7d451
coreazon <normanmatteo@gmail.com>
2025-02-06 21:56:56 +0100

    added: JMZ Command

---
+++ src/model/ai/AIJmzCommand.java
@@ -0,0 +1,15 @@
+package model.ai;
+
+public class AIJmzCommand extends AICommand {
+
+    private static final String JMZ = "JMZ";
+
+    public AIJmzCommand() {
+        super(JMZ);
+    }
+
+    @Override
+    String executeCommand() {
+        return null;
+    }
+}
