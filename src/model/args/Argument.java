3e077881aae1158634512c26d686d7d8310bcb04
coreazon <normanmatteo@gmail.com>
2025-02-06 21:56:21 +0100

    added: equals method

--- src/model/args/Argument.java
+++ src/model/args/Argument.java
@@ -1,5 +1,7 @@
 package model.args;
 
+import java.util.Objects;
+
 public class Argument {
 
     //value goes from min to max | max+1=min; min-1=max
@@ -21,6 +23,19 @@ public class Argument {
     public void add(Argument other) {
         //TODO: handling max+1/min-1
         this.value += other.getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Argument argument = (Argument) o;
+        return value == argument.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 
     @Override
